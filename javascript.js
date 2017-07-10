9393be79ae82bf751c73144df05a62a8209fcdd9
David Castro <davidcastro@Davids-MacBook-Pro.local>
2017-07-10 18:22:37 -0400

    Finished. I need to add a footer and have the button fixed to the same position

--- javascript.js
+++ javascript.js
@@ -1,42 +1,37 @@
 $(document).ready(function() {
-
 	var quote;
 	var author;
 
-	function getNewQuote(){
+	function getNewQuote() {
 		$.ajax({
 			url: 'https://api.forismatic.com/api/1.0/',
-			jsonp:'jsonp',
+			jsonp: 'jsonp',
 			dataType: 'jsonp',
-			data:{
+			data: {
 				method: 'getQuote',
 				lang: 'en',
 				format: 'jsonp'
 			},
-			success: function (response){
+			success: function(response) {
 				quote = response.quoteText;
 				author = response.quoteAuthor;
-			$('#quoteText').text(quote);
+	
+				$('#quoteText').text(quote).hide().fadeIn(700);
+				$('#quoteText').text(" \"" + quote +"\"");
+				
 				//if has an an author
-
-			if(author){
-				$("#quoteAuthor").text('- ' + author);
-			} 
-			else{
-				$("#quoteAuthor").text('-' + Unknown);
+				if (author) {
+					$("#quoteAuthor").text('- ' + author);
+				} else {
+					$("#quoteAuthor").text('- ' + 'Unknown');
+				}
 			}
-
-			}
-
 		});
 	}
-getNewQuote();
-
-$('#quote_Button').on("click", function (event){
-	event.preventDefault();
 	getNewQuote();
-})
+	$('#quote_Button').on("click", function(event) {
 
+		getNewQuote();
+		
+	})
 });
-
-
