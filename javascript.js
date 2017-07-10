2ce9364b5e59c18478648f7446d95f950aab9f55
David Castro <davidcastro@Davids-MacBook-Pro.local>
2017-07-09 23:54:03 -0400

    Finished API functionality

--- javascript.js
+++ javascript.js
@@ -1,10 +1,42 @@
 $(document).ready(function() {
-  $("#quote_Button").on("click", function(){
-     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
-  $("quoteText").html(JSON.stringify(json));
-});
-      
-});
+
+	var quote;
+	var author;
+
+	function getNewQuote(){
+		$.ajax({
+			url: 'https://api.forismatic.com/api/1.0/',
+			jsonp:'jsonp',
+			dataType: 'jsonp',
+			data:{
+				method: 'getQuote',
+				lang: 'en',
+				format: 'jsonp'
+			},
+			success: function (response){
+				quote = response.quoteText;
+				author = response.quoteAuthor;
+			$('#quoteText').text(quote);
+				//if has an an author
+
+			if(author){
+				$("#quoteAuthor").text('- ' + author);
+			} 
+			else{
+				$("#quoteAuthor").text('-' + Unknown);
+			}
+
+			}
+
+		});
+	}
+getNewQuote();
+
+$('#quote_Button').on("click", function (event){
+	event.preventDefault();
+	getNewQuote();
+})
+
 });
 
 
